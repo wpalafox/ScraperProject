8d3e7c458e8c1dbbe2121f215bc06f6b7ccb21f2
Will Palafox <wpalafox@gmail.com>
2017-01-12 23:40:21 -0600

    Frontend, middleware and backend added

---
+++ public/app.js
@@ -0,0 +1,29 @@
+//Collect the articles as JSON objects
+
+function scrapeArticles(){
+	
+	$.getJSON("/articles", function(data){
+		//Remove previously placed articles		
+		$("#articles").empty();
+		
+		//For each one
+		for(var i = 0; i < data.length; i++){
+			var commentsCount;
+			
+			if(data[i].note.length > 0){
+				commentsCount = data[i].note.length;	
+			}
+		
+		
+		//Displaying the apropos
+		$("#articles").append("<p class='title' data-id='" + data[i]._id + "'>" + data[i].title + "  -  <a href='" + data[i].link + "'>Link</a></p>" + "<a data-id='"+ 
+        data[i]._id + "' href='#notes' data-target='notes' class='showNotes modal-trigger waves-effect waves-light btn  blue darken-1'>Comments ("+data[i].note.length + ")</button>");
+      	
+      }
+	
+	});
+
+}
+
+
+scrapeArticles();   
--- server.js
+++ server.js
@@ -98,6 +98,99 @@ app.get("/scrape", function(req, res) {
 });
 
 
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  
+  // TODO: Finish the route so it grabs all of the articles
+  Article.find({}, function(error, article){
+
+    if(error){
+      
+      res.send(error);
+    
+    }
+    
+    else{
+      
+      res.json(article);
+    
+    }
+
+  });
+
+});
+
+// Grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res){
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "note": doc._id }})
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+
+//delete a note route
+app.post("/articles/:id/delete", function(req,res){
+  console.log("the note will delete now");
+  console.log(req.params.id);
+  console.log(req.body);
+  
+  Note.remove({"_id":req.params.id}, function(data){
+    console.log(data)
+  })
+
+  Article.findOneAndUpdate({"_id": req.body.article}, { $pull:{"note": req.params.id}}, function(err){
+    if (err) console.log(err);
+  })
+});
+
+
+
+
+
 
 
 // Listen on port 3000
