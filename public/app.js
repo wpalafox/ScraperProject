66aad89e262e905e9f61fe6b8a5ab243df486337
Will Palafox <wpalafox@gmail.com>
2017-01-14 15:43:06 -0600

    Scrapper and dynamically adding elements

--- public/app.js
+++ public/app.js
@@ -1,29 +1,16 @@
 //Collect the articles as JSON objects
+console.log("test");
 
-function reloadArticles(){
-	
-	$.getJSON("/articles", function(data){
-		//Remove previously placed articles		
-		$("#articles").empty();
-		
-		//For each one
-		for(var i = 0; i < data.length; i++){
-			var commentsCount;
-			
-			if(data[i].note.length > 0){
-				commentsCount = data[i].note.length;	
-			}
-		
-		
-		//Displaying the apropos
-		$("#articles").append("<p class='title' data-id='" + data[i]._id + "'>" + data[i].title + "  -  <a href='" + data[i].link + "'>Link</a></p>" + "<a data-id='"+ 
-        data[i]._id + "' href='#notes' data-target='notes' class='showNotes modal-trigger waves-effect waves-light btn  blue darken-1'>Comments ("+ data[i].note.length + ")</button>");
-      	
-      }
-	
-	});
-
-}
+// Grab the articles as a json 
+$.getJSON("/articles", function (data){
+//For each article
+for(var i = 0; i< data.length; i++){
 
 
-reloadArticles();   
+	$("#contentDisplay").append("<div class= 'panel panel-primary' > <div class='panel-heading'><h4>" + data[i].title + "</h><button data-id='" + data[i]._id + "' class='btn btn-danger' id='save'>Save Article</button></div><div class='panel-body'><a href='"+ data[i].link +"'>"+"Click here for the story"+"</a></div></div>");
+
+
+	}
+});
+
+
